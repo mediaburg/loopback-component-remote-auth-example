0f5dc7a292d65da4acbd3e4c875e282de670413b
Waldemar Zahn <wzahn@mediaburg.ch>
2016-09-21 16:55:00 +0200

    add loopback-component-remote-auth

--- auth/server/boot/authentication.js
+++ auth/server/boot/authentication.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var remoteAuthToken = require('loopback-rest-remote-auth-token');
 module.exports = function (app) {
     app.enableAuth();
-    remoteAuthToken.server(app);
 };
--- data/server/boot/authentication.js
+++ data/server/boot/authentication.js
@@ -1,8 +1,5 @@
 'use strict';
 
-var ctx = require('loopback-context');
-var remoteAuthToken = require('loopback-rest-remote-auth-token');
 module.exports = function (app) {
     app.enableAuth();
-    remoteAuthToken.client(app, ctx);
 };
